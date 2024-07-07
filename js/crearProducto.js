a8bf57adf59d5d22bd2c93ac10b62118336cad6e
fernando-hess <dhofher.hess@gmail.com>
2024-07-06 21:30:07 -0600

    Corrige verificacion de url

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -6,54 +6,42 @@ async function crearProducto (evento) {
 
     evento.preventDefault();
 
+    // Validación de la URL
+    const urlInput = document.getElementById('image-url');
+    const url2 = urlInput.value.toLowerCase().trim();
+
+    const imageUrlPattern = /\.(jpeg|jpg|gif|png|bmp|webp)/i;
+    const imageKeywords = /(images?|photos?)/;
+
+    if (!imageUrlPattern.test(url2) && !imageKeywords.test(url2)) {
+        alert('La URL ingresada no es válida. Por favor, ingrese una URL de imagen válida.');
+        return; // Deteniene la ejecución si la url no es valida
+    } else {
     const titulo = document.querySelector("[data-titulo]").value;
     const url = document.querySelector("[data-url]").value;
     const descripcion = document.querySelector("[data-descripcion]").value;
 
-    // prueba
     function formatearPrecio(precio) {
          return new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN' }).format(precio);
      }
 
-
     const precioFormateado = formatearPrecio(descripcion);
 
      await conexionAPI.enviarProducto(titulo, precioFormateado, url); 
-    //await conexionAPI.enviarProducto(titulo, descripcion, url);
     
-    // console.log("Se creo producto")
-
-    //window.location.href="../index.html"
-
+    
+    alert('Producto creado exitosamente.');
     window.location.reload();
-
+    }
 }
 
 formulario.addEventListener("submit", evento => crearProducto(evento));
-
-// document.addEventListener('click', (event) => {
-//     if (event.target.closest('.producto__borrar')) {
-//         alert('¡Botón borrado ha sido clickeado!');
-//     }
-// });
-
-// Agrega el event listener a los botones después de insertarlos
-// const botonesBorrar = document.querySelectorAll('.producto__borrar');
-// botonesBorrar.forEach(boton => {
-//     boton.addEventListener('click', (event) => {
-//         const productoElement = event.target.closest('.producto');
-//         if (productoElement) {
-//             productoElement.remove(); // Eliminar el elemento del DOM
-//             alert('¡Producto eliminado!');
-//         }
-//     });
-// });
 
 document.querySelector('.productos__container').addEventListener('click', (event) => {
     if (event.target.closest('.producto__borrar')) {
         const productoElement = event.target.closest('.producto');
         if (productoElement) {
-            productoElement.remove(); // Eliminar el elemento del DOM
+            productoElement.remove(); // Elimina el elemento del DOM
             alert('¡Producto eliminado!');
         }
     }
